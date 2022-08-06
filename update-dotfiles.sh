76da377b9d4801b553f7c409d891eff97febcf7a
Quinn Yockey <qyockey@tutanota.de>
2022-08-06 09:34:40 -0700

    added color to update script

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -9,31 +9,31 @@ echo ""
 
 echo -e "${GREEN}copying files into ./alacritty/${NOCOLOR}"
 cp -r ~/.config/alacritty/*  ./alacritty
-echo "${GREEN}copying files into ./awesome/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./awesome/${NOCOLOR}"
 cp -r ~/.config/awesome/*    ./awesome
-echo "${GREEN}copying files into ./bash/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./bash/${NOCOLOR}"
 cp -r ~/.bashrc              ./bash
-echo "${GREEN}copying files into ./emacs/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./emacs/${NOCOLOR}"
 cp -r ~/.doom.d              ./emacs
-echo "${GREEN}copying files into ./i3/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./i3/${NOCOLOR}"
 cp -r ~/.config/i3/*         ./i3
-echo "${GREEN}copying files into ./neofetch/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./neofetch/${NOCOLOR}"
 cp -r ~/.config/neofetch/*   ./neofetch
-echo "${GREEN}copying files into ./nvim/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./nvim/${NOCOLOR}"
 cp -r ~/.config/nvim/*       ./nvim
-echo "${GREEN}copying files into ./polybar/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./polybar/${NOCOLOR}"
 cp -r /etc/polybar/*         ./polybar
-echo "${GREEN}copying files into ./rofi/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./rofi/${NOCOLOR}"
 cp -r ~/.config/rofi/*       ./rofi
-echo "${GREEN}copying files into ./vim/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./vim/${NOCOLOR}"
 cp -r ~/.vimrc               ./vim
-echo "${GREEN}copying files into ./zsh/${NOCOLOR}"
+echo -e "${GREEN}copying files into ./zsh/${NOCOLOR}"
 cp -r ~/.zshrc               ./zsh
 
-echo "${GREEN}adding all files for commit${NOCOLOR}"
+echo -e "${GREEN}adding all files for commit${NOCOLOR}"
 git add -A
-echo "${GREEN}commiting${NOCOLOR}"
+echo -e "${GREEN}commiting${NOCOLOR}"
 git commit -m "$message"
-echo "${GREEN}pushing changes${NOCOLOR}"
+echo -e "${GREEN}pushing changes${NOCOLOR}"
 git push
-echo "${GREEN}done!${NOCOLOR}"
+echo -e "${GREEN}done!${NOCOLOR}"
